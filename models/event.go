4cc7b5ffe602cf21c1cb7c02092af4d0c972b5e0
Ali Assar <150648843+Ali-Assar@users.noreply.github.com>
2023-12-26 12:46:51 +0000

    adding get and post

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+	"github.com/Ali-Assar/go-REST-Api.git/db"
+	"database/sql"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding: "required"`
 	Description string    `binding: "required"`
 	Location    string    `binding: "required"`
@@ -15,9 +19,63 @@ var events = []Event{}
 
 func (e Event) Save() {
 	// Database will be added later
+	query:=`
+	INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?,?,?,?,?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	
+	if err!=nil{
+		return err
+	}
+	defer stmt.Close()
+
+	result,err:= stmt.Exec(e.Name,e.Description,e.Location,e.DateTime,e.UserID)
+	
+	if err!=nil{
+		return err
+	}
+
+	id,err:=result.LastInserId()
+	e.Id =id
+	return err
+
+
 	events = append(events, e)
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event,error) {
+	query :="SELECT * FROM events"
+	rows, err:= db.DB.Query(query)
+	if err!=nil{
+		return nil,err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next(){
+		var event Event
+		err :=rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		
+		if err!=nil{
+			return nil,err
+		}
+
+		events=append(events,event)
+	}
+
+	return events, nil
+}
+
+func GetEventByID(id int64)(*Event,error){
+	query:= "SELECT * FROM events WHERE id = ?"
+	row:=db.DB.QueryRow(query, id)
+
+	var event event
+	err :=rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err!=nil{
+		return nil,err
+	}
+	return &Event,nil
 }
