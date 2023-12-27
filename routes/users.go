747aec10c2edd900790d38b119b4e6f62e1d9f6d
ali <ali.assar@skiff.com>
2023-12-27 20:33:45 +0330

    adding jwt

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Ali-Assar/go-REST-Api.git/models"
+	"github.com/Ali-Assar/go-REST-Api.git/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -25,4 +26,28 @@ func signup(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "user created successfully."})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "login successful!", "token": token})
+
 }
